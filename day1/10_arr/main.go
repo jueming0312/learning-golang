bb20d37829d5356c02d0e38527dfeb1e04d86687
jueming <jeming@cobra37.com>
2023-08-04 18:17:47 +0800

    练习数组循环

--- day1/10_arr/main.go
+++ day1/10_arr/main.go
@@ -12,4 +12,14 @@ func main() {
 	b[2] = 9
 	fmt.Println(a)
 	fmt.Println(b)
+
+	var num1 = [3]int{1, 2, 3}
+	var num2 = [...]string{"tom", "jack", "mary"}
+	num3 := [3]float64{1.23, 3.24, 56.11}
+	fmt.Println(num1, num2, num3)
+
+	for i, v := range num3 {
+		fmt.Printf("index=%d value=%v\n", i, v)
+	}
+
 }
